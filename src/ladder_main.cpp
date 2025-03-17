6f145bef427430ede1eef1fa3866b20ec11ec2ec
Aaron Chen <xiyuec5@uci.edu>
2025-03-17 01:41:05 +0000

    debug main

--- src/ladder_main.cpp
+++ src/ladder_main.cpp
@@ -11,8 +11,18 @@ int main(int argc, char* argv[]) {
         load_words(word_list, argv[1]);
         string start_word = argv[2];
         string end_word = argv[3];
+        
         transform(start_word.begin(), start_word.end(), start_word.begin(), ::tolower);
         transform(end_word.begin(), end_word.end(), end_word.begin(), ::tolower);
+        
+        if (start_word == end_word) {
+            cout << "Word ladder found: " << start_word << " " << endl;
+            return 0;
+        }
+        
+        if (word_list.find(end_word) == word_list.end()) {
+            error(start_word, end_word, "End word must be in dictionary");
+        }
         
         vector<string> ladder = generate_word_ladder(start_word, end_word, word_list);
         print_word_ladder(ladder);
