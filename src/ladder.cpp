05513a39a8984a2e81d2c18c2d4366fc2cb60aec
Aaron Chen <xiyuec5@uci.edu>
2025-03-17 02:21:31 +0000

    test on dijkstras

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -1,99 +1,99 @@
-#include "ladder.h"
+// #include "ladder.h"
 
-bool edit_distance_within(const string& str1, const string& str2, int d) {
-    if (str1 == str2) return true; 
+// bool edit_distance_within(const string& str1, const string& str2, int d) {
+//     if (str1 == str2) return true; 
     
-    int len1 = str1.length();
-    int len2 = str2.length();
+//     int len1 = str1.length();
+//     int len2 = str2.length();
     
-    if (abs(len1 - len2) > d) return false;
+//     if (abs(len1 - len2) > d) return false;
     
-    if (len1 == len2) {
-        int differences = 0;
-        for (size_t i = 0; i < len1; i++) {
-            if (str1[i] != str2[i]) differences++;
-            if (differences > d) return false;
-        }
-        return true;
-    }
+//     if (len1 == len2) {
+//         int differences = 0;
+//         for (size_t i = 0; i < len1; i++) {
+//             if (str1[i] != str2[i]) differences++;
+//             if (differences > d) return false;
+//         }
+//         return true;
+//     }
     
-    const string& shorter = (len1 < len2) ? str1 : str2;
-    const string& longer = (len1 < len2) ? str2 : str1;
+//     const string& shorter = (len1 < len2) ? str1 : str2;
+//     const string& longer = (len1 < len2) ? str2 : str1;
     
-    for (size_t i = 0; i <= longer.length(); i++) {
-        string temp = longer;
-        temp.erase(i, 1);
-        if (temp == shorter) return true;
-    }
+//     for (size_t i = 0; i <= longer.length(); i++) {
+//         string temp = longer;
+//         temp.erase(i, 1);
+//         if (temp == shorter) return true;
+//     }
     
-    return false;
-}
+//     return false;
+// }
 
-bool is_adjacent(const string& word1, const string& word2) {
-    if (word1 == word2) return true; 
-    return edit_distance_within(word1, word2, 1);
-}
+// bool is_adjacent(const string& word1, const string& word2) {
+//     if (word1 == word2) return true; 
+//     return edit_distance_within(word1, word2, 1);
+// }
 
-vector<string> generate_word_ladder(const string& begin_word, const string& end_word, const set<string>& word_list) {
-    if (begin_word == end_word) {
-        return vector<string>();  
-    }
+// vector<string> generate_word_ladder(const string& begin_word, const string& end_word, const set<string>& word_list) {
+//     if (begin_word == end_word) {
+//         return vector<string>();  
+//     }
     
-    queue<vector<string>> paths;
-    set<string> visited;
+//     queue<vector<string>> paths;
+//     set<string> visited;
     
-    paths.push({begin_word});
-    visited.insert(begin_word);
+//     paths.push({begin_word});
+//     visited.insert(begin_word);
     
-    while (!paths.empty()) {
-        vector<string> current_path = paths.front();
-        paths.pop();
+//     while (!paths.empty()) {
+//         vector<string> current_path = paths.front();
+//         paths.pop();
         
-        string current = current_path.back();
+//         string current = current_path.back();
         
-        for (const string& word : word_list) {
-            if (visited.count(word)) continue;
+//         for (const string& word : word_list) {
+//             if (visited.count(word)) continue;
             
-            if (is_adjacent(current, word)) {
-                vector<string> new_path = current_path;
-                new_path.push_back(word);
+//             if (is_adjacent(current, word)) {
+//                 vector<string> new_path = current_path;
+//                 new_path.push_back(word);
                 
-                if (word == end_word) {
-                    return new_path;
-                }
+//                 if (word == end_word) {
+//                     return new_path;
+//                 }
                 
-                visited.insert(word);
-                paths.push(new_path);
-            }
-        }
-    }
+//                 visited.insert(word);
+//                 paths.push(new_path);
+//             }
+//         }
+//     }
     
-    return vector<string>();
-}
+//     return vector<string>();
+// }
 
-void load_words(set<string>& word_list, const string& file_name) {
-    ifstream file(file_name);
-    if (!file) {
-        throw runtime_error("Cannot open dictionary file: " + file_name);
-    }
+// void load_words(set<string>& word_list, const string& file_name) {
+//     ifstream file(file_name);
+//     if (!file) {
+//         throw runtime_error("Cannot open dictionary file: " + file_name);
+//     }
     
-    string word;
-    while (file >> word) {
-        transform(word.begin(), word.end(), word.begin(), ::tolower);
-        word_list.insert(word);
-    }
-}
+//     string word;
+//     while (file >> word) {
+//         transform(word.begin(), word.end(), word.begin(), ::tolower);
+//         word_list.insert(word);
+//     }
+// }
 
-void print_word_ladder(const vector<string>& ladder) {
-    if (ladder.empty()) {
-        cout << "No word ladder found." << endl;
-        return;
-    }
+// void print_word_ladder(const vector<string>& ladder) {
+//     if (ladder.empty()) {
+//         cout << "No word ladder found." << endl;
+//         return;
+//     }
     
-    cout << "Word ladder found: ";
-    for (size_t i = 0; i < ladder.size(); i++) {
-        cout << ladder[i];
-        if (i < ladder.size() - 1) cout << " ";
-    }
-    cout << " " << endl;
-}
+//     cout << "Word ladder found: ";
+//     for (size_t i = 0; i < ladder.size(); i++) {
+//         cout << ladder[i];
+//         if (i < ladder.size() - 1) cout << " ";
+//     }
+//     cout << " " << endl;
+// }
--- src/ladder_main.cpp
+++ src/ladder_main.cpp
@@ -1,36 +1,36 @@
-#include "ladder.h"
+// #include "ladder.h"
 
-int main(int argc, char* argv[]) {
-    if (argc != 4) {
-        cerr << "Usage: " << argv[0] << " <dictionary_file> <start_word> <end_word>" << endl;
-        return 1;
-    }
+// int main(int argc, char* argv[]) {
+//     if (argc != 4) {
+//         cerr << "Usage: " << argv[0] << " <dictionary_file> <start_word> <end_word>" << endl;
+//         return 1;
+//     }
 
-    try {
-        set<string> word_list;
-        load_words(word_list, argv[1]);
-        string start_word = argv[2];
-        string end_word = argv[3];
+//     try {
+//         set<string> word_list;
+//         load_words(word_list, argv[1]);
+//         string start_word = argv[2];
+//         string end_word = argv[3];
         
-        transform(start_word.begin(), start_word.end(), start_word.begin(), ::tolower);
-        transform(end_word.begin(), end_word.end(), end_word.begin(), ::tolower);
+//         transform(start_word.begin(), start_word.end(), start_word.begin(), ::tolower);
+//         transform(end_word.begin(), end_word.end(), end_word.begin(), ::tolower);
         
-        if (start_word == end_word) {
-            cout << "Word ladder found: " << start_word << " " << endl;
-            return 0;
-        }
+//         if (start_word == end_word) {
+//             cout << "Word ladder found: " << start_word << " " << endl;
+//             return 0;
+//         }
         
-        if (word_list.find(end_word) == word_list.end()) {
-            error(start_word, end_word, "End word must be in dictionary");
-        }
+//         if (word_list.find(end_word) == word_list.end()) {
+//             error(start_word, end_word, "End word must be in dictionary");
+//         }
         
-        vector<string> ladder = generate_word_ladder(start_word, end_word, word_list);
-        print_word_ladder(ladder);
+//         vector<string> ladder = generate_word_ladder(start_word, end_word, word_list);
+//         print_word_ladder(ladder);
         
-    } catch (const exception& e) {
-        cerr << e.what() << endl;
-        return 1;
-    }
+//     } catch (const exception& e) {
+//         cerr << e.what() << endl;
+//         return 1;
+//     }
     
-    return 0;
-}
+//     return 0;
+// }
